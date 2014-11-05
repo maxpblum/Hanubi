1efc20d8cc32d24b00d26c18544ee920e8fe8972
Max Blum <max.blum@gmail.com>
2014-11-05 15:45:21 -0500

    Added Chat and Rename functionality in-game. On the backend.

--- hserver.js
+++ hserver.js
@@ -6,33 +6,56 @@ var _    = require('underscore');
 var userHandler = require('./users.js')(io);
 var gameServer = require('./gserver.js')(io);
 
+function makeRename(user, group) {
+
+  return function(newName) {
+
+    console.warn("User '"+ user.name + "' changed name to '" + newName + "'");
+    user.name = newName;
+    group.emitNames();
+
+  };
+
+}
+
+function makeChat(user, group) {
+
+  return function(text) {
+
+    group.sendAll('chat', {
+      name: user.name,
+      text: text
+    });
+    console.log("User '" + user.name + "' said '" + text + "'");
+
+  };
+
+}
+
+function makeStartGame(user, group) {
+
+  return function() {
+    var gameID = gameServer.createGame(group.users);   
+
+    var userMessages    = gameServer.userMessages(gameID);
+    var groupMethods    = gameServer.groupMethods(gameID);
+    userMessages.rename = makeRename;
+    userMessages.chat   = makeChat;
+
+    var gameGroup = userHandler.makeGroup(gameID, 
+                                          userMessages, 
+                                          groupMethods);
+    group.sendAll('gameIsStarting', gameID);
+    group.clearUsers();
+  };
+
+}
+
 var chatGroup = userHandler.makeGroup(
   'chat', 
   { // User message methods
-    rename: function(user, group) {
-      return function(newName) {
-        console.warn("User '"+ user.name + "' changed name to '" + newName + "'");
-        user.name = newName;
-        group.emitNames();
-      };
-    },
-    startGame: function(user, group) {
-      return function() {
-        var gameID = gameServer.createGame(group.users);   
-        var gameGroup = userHandler.makeGroup(gameID, 
-                                              gameServer.userMessages(gameID), 
-                                              gameServer.groupMethods(gameID));
-        group.sendAll('gameIsStarting', gameID);
-        group.clearUsers();
-      };
-    },
-    chat: function(user, group) {
-      return function(text) {
-        group.sendAll('chat', {
-          name: user.name,
-          text: text
-        });
-        console.log("User '" + user.name + "' said '" + text + "'");
-      };
+    rename: makeRename,
+    startGame: makeStartGame,
+    chat: makeChat
   }
-});
+);
