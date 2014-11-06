306d4350969ae3482d123d821a988b41f26a0ac6
Marcin Skirzynski <marcin.skirzynski@gmail.com>
2014-11-05 22:26:11 -0500

    Clues are displayed now -- finally

--- public/js/chat.jsx
+++ public/js/chat.jsx
@@ -13,20 +13,25 @@ var app = app || {};
 
   app.Chat = React.createClass({
     getInitialState: function() {
-      this.props.socket.on('chat', function(message) {
-        var messages = this.state.messages;
-        messages.push(message);
-        this.setState({messages: messages});
-        // Scroll to bottom to see the new message
-        var wrapper = document.getElementsByClassName("overflow-wrapper")[0];
-        wrapper.scrollTop = wrapper.scrollHeight;
-      }.bind(this));
+      this.props.socket.on('chat', this.flushMessage.bind(this));
       return {message: "", messages: []}
     },
-    show: function(message) {
-      return [<dt>{message.name}</dt>,
-              <dd>{message.text}</dd>]
+    
+    flushMessage: function(message) {
+      var messages = this.state.messages;
+      messages.push(message);
+      this.setState({messages: messages});
+      // Scroll to bottom to see the new message
+      var wrapper = document.getElementsByClassName("overflow-wrapper")[0];
+      wrapper.scrollTop = wrapper.scrollHeight;
     },
+
+    showClue: function(clue) {
+      console.log("FLUSHING");
+      this.flushMessage({type: "clue", clue: clue });
+
+    },
+
     emitMessage: function(e) {
       e.preventDefault();
       var message = this.state.message;
@@ -38,10 +43,45 @@ var app = app || {};
       this.setState({message: message});
     },
     render: function() {
+      var show = function(message) {
+        if(message.type === 'clue') {
+          console.log("Clue received " + message.clue.suitOrNumber);
+          console.log(message.clue);
+          var cards = [];
+          for(var i=0; i<5; i++) {
+            var value    = message.clue.suitOrNumber;
+            var matching = message.clue.matching;
+            
+            var hit      = _.contains(matching, i);
+
+            var suit     = "unknown";
+            var number   = "?";
+            if(parseInt(value) && hit) {
+              number = parseInt(value);
+            } else if (hit) {
+              suit   = value;
+            }
+            cards.push({suit:suit, number: number});
+          }
+          var renderClueCard = function(card) {
+            var classes = "clueCard " + card.suit + "Card";
+            return <div className={classes}>{card.number}</div>
+          };
+          return [<dt>{message.clue.recipient} received a clue from {message.clue.giver}</dt>, 
+                  <dd>
+                    <div className='clueChat'>
+                      {cards.map(renderClueCard)}
+                    </div>
+                  </dd>]
+        } else {
+          return [<dt>{message.name}</dt>,
+                  <dd>{message.text}</dd>]
+        }
+      };
       return (
         <div id='chat'>
           <div className='overflow-wrapper'>
-            <dl>{this.state.messages.map(this.show)}</dl>
+            <dl>{this.state.messages.map(show)}</dl>
           </div>
           <form onSubmit={this.emitMessage}>
             <input id='chat-message' onChange={this.onChange} value={this.state.message} />
